f828110a693064e93227ef49adb444a9cceb60f9
kaisei-2026 <sota3428@gmail.com>
2026-05-14 11:17:36 +0900

    Add blog post data retrieval functions

---
+++ lib/blog.ts
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const postsDirectory = path.join(process.cwd(), 'content');
+
+export function getSortedPostsData() {
+  if (!fs.existsSync(postsDirectory)) return [];
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.filter(fileName => fileName.endsWith('.md')).map((fileName) => {
+    const slug = fileName.replace(/\.md$/, '');
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+    const matterResult = matter(fileContents);
+    return { slug, ...(matterResult.data as { date: string; title: string }) };
+  });
+
+  return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
+}
+
+export function getPostData(slug: string) {
+  const fullPath = path.join(postsDirectory, `${slug}.md`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+  const matterResult = matter(fileContents);
+  return { slug, content: matterResult.content, ...(matterResult.data as { date: string; title: string }) };
+}
