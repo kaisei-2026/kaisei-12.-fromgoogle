8416a0ad309da378f5672d592535d77043dcd263
kaisei-2026 <sota3428@gmail.com>
2026-05-14 22:54:27 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/kaisei-google',
+  basePath: '/kaisei-google', // 🌟 リポジトリ名に合わせる
   images: { unoptimized: true },
-  // 🌟 ここから下の3行を追加：エラーがあっても無視して公開する設定
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  swcMinify: true,
 };
 
 export default nextConfig;
