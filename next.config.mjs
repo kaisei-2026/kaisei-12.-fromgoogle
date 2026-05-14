015b4adcec1f86dceca709e0964f12fd30a0c87d
kaisei-2026 <sota3428@gmail.com>
2026-05-14 12:05:49 +0900

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Next.jsを静的なHTMLに変換する魔法
+  basePath: '/kaisei-12.-fromgoogle', // GitHubのリポジトリ名
+};
+
+export default nextConfig;
