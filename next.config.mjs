b70f413334bf5faa69b1b7fe1472a6dda9c62f61
kaisei-2026 <sota3428@gmail.com>
2026-05-17 00:32:45 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/kaisei-google', // 🌟 リポジトリ名に合わせる
+  basePath: '/kaisei-google',
   images: { unoptimized: true },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  // 🌟 ここから下を追加！
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
